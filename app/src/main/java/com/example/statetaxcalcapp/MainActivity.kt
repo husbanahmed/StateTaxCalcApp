3e0e898ce532f97a66c80c13aa0a210d8473f8ad
Husban Ahmed <husban13@gmail.com>
2023-06-13 23:43:58 -0700

    Local Permission Progress

--- app/src/main/java/com/example/statetaxcalcapp/MainActivity.kt
+++ app/src/main/java/com/example/statetaxcalcapp/MainActivity.kt
@@ -2,15 +2,11 @@ package com.example.statetaxcalcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import android.Manifest
-import android.content.pm.PackageManager
 import com.example.statetaxcalcapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +18,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setCurrentFragment(localFragment)
 
+
         binding.bottomNavigationView.setOnItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.state_tax -> setCurrentFragment(stateFragment)
                 R.id.local_tax -> setCurrentFragment(localFragment)
+
             }
             true
         }
@@ -36,23 +34,4 @@ class MainActivity : AppCompatActivity() {
             replace(R.id.frameLayout,fragment)
             commit()
         }
-
-    private fun hasLocationForegroundPermission() =
-        ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-
-    private fun hasLocationFinePermission() =
-        ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-
-    private fun requestPermissions() {
-        var permissionToRequest= mutableListOf<String>()
-        if(!hasLocationForegroundPermission()){
-            permissionToRequest.add(Manifest.permission.ACCESS_COARSE_LOCATION)
-        }
-        if(!hasLocationFinePermission()){
-            permissionToRequest.add(Manifest.permission.ACCESS_FINE_LOCATION)
-        }
-    }
-
-
-
 }
